89491a13a971f5358dd0f7457aa89083da08f75d
Alister Burt <alisterburt@gmail.com>
2020-06-13 11:53:20 +0200

    add imports in __init__.py

--- eulerangles/__init__.py
+++ eulerangles/__init__.py
@@ -1,1 +1,5 @@
-
+from .conventions import EulerAngleConvention, EMEulerAngleConvention
+from .conversions import euler2matrix, euler2euler
+from .eulers import EulerAnglesRelion, EulerAnglesDynamo, EulerAnglesEmclarity, EulerAnglesM, EulerAnglesPeet, \
+    EulerAnglesWarp, EulerAnglesEmclarity
+from .rotations import RotX, RotY, RotZ
