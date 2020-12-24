fix yzy extrinsic conversion (was missing a minus sign in the call to atan2)

eulerangles/conversions.py
@@ -213,7 +213,7 @@ def matrix2yzy_extrinsic(rotation_matrices: np.ndarray) -> np.ndarray:
     r32 = rotation_matrices[idx, 2, 1]
     r12 = rotation_matrices[idx, 0, 1]
     angles_radians[idx, 0] = np.arctan2(r23, r21)
-    angles_radians[idx, 2] = np.arctan2(r32, r12)
+    angles_radians[idx, 2] = np.arctan2(r32, -r12)
 
     # convert to degrees
     euler_angles = np.rad2deg(angles_radians)