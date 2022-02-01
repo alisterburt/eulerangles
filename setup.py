301454c5ce7578cabc2db439ff0c70691e3d9e1e
Alister Burt <alisterburt@gmail.com>
2022-02-01 21:56:20 +0000

    update setup workflow

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-from setuptools import setup, find_packages
+import setuptools
 
-from eulerangles.version import __version__
-
-setup(
-    name='eulerangles',
-    packages=find_packages(include=['eulerangles', '*'], exclude=['test',]),
-    version=f'{__version__}',
-    license='BSD 3-Clause License',
-    description="""Simplify the handling of large sets of Euler angles in Python
-    """,
-    author='Alister Burt',
-    author_email='alisterburt@gmail.com',
-    url='https://github.com/alisterburt/eulerangles',
-    keywords=['transformation', 'euler angles', 'eulerian angles', 'cryo-EM', 'cryo-ET'],
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-)
+setuptools.setup(use_scm_version={"write_to": "eulerangles/_version.py"})
